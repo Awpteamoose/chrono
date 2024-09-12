607e3f6b8eaabdcd7bdd0a362ee96116a798b4c6
Awpteamoose <awpteamoose@gmail.com>
2024-09-12 14:09:24 +0100

    fix everything

--- src/time_delta.rs
+++ src/time_delta.rs
@@ -53,10 +53,11 @@ const SECS_PER_WEEK: i64 = 604_800;
 #[cfg_attr(
     any(feature = "rkyv", feature = "rkyv-16", feature = "rkyv-32", feature = "rkyv-64"),
     derive(Archive, Deserialize, Serialize),
-    archive(compare(PartialEq, PartialOrd)),
-    archive_attr(derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash))
+	rkyv(
+		compare(PartialEq, PartialOrd),
+		derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash),
+	),
 )]
-#[cfg_attr(feature = "rkyv-validation", archive(check_bytes))]
 pub struct TimeDelta {
     secs: i64,
     nanos: i32, // Always 0 <= nanos < NANOS_PER_SEC
@@ -1350,7 +1351,7 @@ mod tests {
     #[cfg(feature = "rkyv-validation")]
     fn test_rkyv_validation() {
         let duration = TimeDelta::try_seconds(1).unwrap();
-        let bytes = rkyv::to_bytes::<_, 16>(&duration).unwrap();
-        assert_eq!(rkyv::from_bytes::<TimeDelta>(&bytes).unwrap(), duration);
+        let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&duration).unwrap();
+        assert_eq!(rkyv::from_bytes::<TimeDelta, rkyv::rancor::Error>(&bytes).unwrap(), duration);
     }
 }
